42b19f5726e4dad6b258ede47867d9312a778b49
Nicholas Jamieson <nicholas@cartant.com>
2020-08-13 14:47:09 +1000

    chore: Update comment links.

--- tests/utils.ts
+++ tests/utils.ts
@@ -1,6 +1,6 @@
 /**
  * @license Use of this source code is governed by an MIT-style license that
- * can be found in the LICENSE file at https://github.com/cartant/eslint-plugin-etc
+ * can be found in the LICENSE file at https://github.com/cartant/eslint-plugin-dtslint
  */
 
 import { createRuleTester } from "eslint-etc";
